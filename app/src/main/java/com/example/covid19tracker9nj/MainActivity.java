d11e3573a7b8de3bb17451d0c2f21ebfdc7e85cf
Azwel Simwinga <mulumbesimwinga@gmail.com>
2021-04-12 14:55:09 +0200

    App Completed

--- app/src/main/java/com/example/covid19tracker9nj/MainActivity.java
+++ app/src/main/java/com/example/covid19tracker9nj/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -11,6 +12,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,13 +34,15 @@ public class MainActivity extends AppCompatActivity {
         tvTotalDeaths = findViewById(R.id.tvTotalDeaths);
         tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
         tvAffectedCountries = findViewById(R.id.tvAffectedCountries);
+
+        fetchdata();
     }
 
     private void fetchdata(){
 
         // Create a String request using Volley Library
 
-        String url = "https:// corona.lmao.ninja/v2/all";
+        String url = "https://disease.sh/v3/covid-19/all";
 
         StringRequest request
                 = new StringRequest(
@@ -46,14 +52,52 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
 
+                        // Handle the JSON object and handle it inside try and catch
+
+                        try {
+                            // Creating object of JSONObject
+                            JSONObject jsonObject
+                                    = new JSONObject(
+                                    response.toString());
+                            // Set the data in text view
+                            // which are available in JSON format
+                            // Note that the parameter
+                            // inside the getString() must match
+                            // with the name given in JSON format
+                            tvCases.setText(
+                                    jsonObject.getString("cases"));
+                            tvRecovered.setText(
+                                    jsonObject.getString("recovered"));
+                            tvCritical.setText(
+                                    jsonObject.getString("critical"));
+                            tvActive.setText(
+                                    jsonObject.getString("active"));
+                            tvTodayCases.setText(
+                                    jsonObject.getString("todayCases"));
+                            tvTotalDeaths.setText(
+                                    jsonObject.getString("deaths"));
+                            tvTodayDeaths.setText(
+                                    jsonObject.getString("todayDeaths"));
+                            tvAffectedCountries.setText(
+                                    jsonObject.getString("affectedCountries"));
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 },
                 new Response.ErrorListener(){
                     @Override
                     public void onErrorResponse(VolleyError error){
-
+                        Toast.makeText(MainActivity.this,
+                                error.getMessage(),
+                                Toast.LENGTH_SHORT)
+                                .show();
                     }
                 }
         );
+        RequestQueue requestQueue
+                = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
